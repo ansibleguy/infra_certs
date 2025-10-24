922355de8a07db3fdc587dfa1d5ce7cebeecc760
Rath Pascal <rath@oxl.at>
2025-10-25 01:22:38 +0200

    minor fixes

--- filter_plugins/utils.py
+++ filter_plugins/utils.py
@@ -111,6 +111,9 @@ class FilterModule(object):
     @staticmethod
     def ensure_list(data: (str, dict, list)) -> list:
         # if user supplied a string instead of a list => convert it to match our expectations
+        if data is None:
+            return []
+
         if isinstance(data, list):
             return data
 
